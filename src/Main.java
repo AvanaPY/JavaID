9b7120b67bb14fe5bfe561f1254bd8178bcc9c2b
Emil Karlström <emil.karlstrom@gmail.com>
2024-10-19 21:30:37 +0200

    Added a worse calculator
    Signed-off-by: Emil Karlström <emil.karlstrom@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ import dicontainer.RegisterType;
 import services.calculations.*;
 import services.calculator.*;
 
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println(System.getProperty("java.version"));
@@ -12,10 +14,11 @@ public class Main {
         container.Register(MultiplicationCalculatorService.class, RegisterType.Singleton);
         container.Register(DivisionCalculationService.class, RegisterType.Singleton);
         container.Register(ExponentialCalculationService.class, RegisterType.Singleton);
-        container.Register(ICalculator.class, Calculator.class, RegisterType.Singleton);
+//        container.Register(ICalculator.class, Calculator.class, RegisterType.Singleton);
+        container.Register(ICalculator.class, AWorseCalculator.class, RegisterType.Singleton);
 
         var calculator = container.ResolveInstance(ICalculator.class);
         var res = calculator.Calculate(2, 3, 2);
-        System.out.println("Calculated: " + res);
+        System.out.println("Calculated result: " + res);
     }
 }
